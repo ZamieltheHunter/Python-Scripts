eaad21058b653d05e24ce9755a2693baf1fae823
Christopher Barden <christopherbarden@yahoo.com>
2018-11-03 16:16:39 -0400

    Finished moving over to pygubu setup.

--- nameGen/nameGen.py
+++ nameGen/nameGen.py
@@ -1,117 +1,252 @@
 #!/usr/bin/python3
 import tkinter as tk
+import json
 import pygubu
 from npc import *
 from random import randint
 import csv
 import sys
+import copy
+from pprint import pprint
 
 class Application:
-    def __init__(self):
+    def __init__(self, window, callbacks, title = None):
         self.builder = builder = pygubu.Builder()
-        builder.add_from_file('ttkLayout.ui')
-        self.mainwindow = builder.get_object('baseWindow')
-        builder.connect_callbacks(self)
+        self.builder.add_from_file(window + '.ui')
+        self.mainwindow = self.builder.get_object(window)
+        if title:
+            self.mainwindow.title(title)
+        self.builder.connect_callbacks(callbacks)
+
     def run(self):
         self.mainwindow.mainloop()
     def quit(self):
-        self.mainwindow.quit()
+        self.mainwindow.destroy()
+
+class RootWindow(Application):
+    def __init__(self):
+        super().__init__('baseWindow', {
+            'saveClicked' : self.save,
+            'deleteClicked': self.deleteNPC,
+            'editClicked': self.editNPC,
+            'displayClicked': self.useNPC,
+            'addNPCClicked': self.addNPC,
+            'newNameClicked': self.makeName,
+            'setGenderBox': self.setGenderBox
+            }, "Fantasy Craft Name Generator")
+        self.builder.get_object('genderBox').current(0)
+        self.builder.get_object('speciesBox').current(0)
+        self.genderBox = self.builder.get_object('genderBox')
+        self.speciesBox = self.builder.get_object('speciesBox')
+        self.nameBox = self.builder.get_object('nameBox')
+        self.listBox = self.builder.get_object('listBox')
+        self.children = {}
+        for name,npc in npcs.items():
+            self.listBox.insert("end", name)
+            pprint(npc)
+            npcs[name] = NPC(name, npc)
+        self.mainwindow.protocol("WM_DELETE_WINDOW", self.onClose)
+
     def setGenderBox(self, event=None):
-        print("Here")
-        genderBox = app.builder.get_object('genderBox')
-        speciesBox = app.builder.get_object('speciesBox')
-        if (speciesBox.get() in ['Rootwalker', 'Unborn', 'Drake']):
-            genderBox.set('Both/Either/Other/None')
-            genderBox.state(['disabled'])
+        if (self.speciesBox.get() in ['Rootwalker', 'Unborn', 'Drake']):
+            self.genderBox.set('Both/Either/Other/None')
+            self.genderBox.state(['disabled'])
         else:
-            genderBox.state(['!disabled'])
+            self.genderBox.state(['readonly'])
+
+    def addNPC(self):
+        name = self.nameBox.get()
+        npcs[name] = NPC(name, {"gender" : self.genderBox.get(), "species" : self.speciesBox.get()})
+        self.listBox.insert("end", name)
+
+    def deleteNPC(self):
+        for name in self.listBox.curselection():
+            self.dl = self.listBox.curselection()
+            messagebox = MessageWindow("Delete " + self.listBox.get(name) + "?", self.remove, None, "Delete?", "Delete", "Cancel")
+
+    def remove(self):
+        del npcs[self.listBox.get(self.dl)]
+        self.listBox.delete(self.dl)
+        self.dl = None
+
+    def editNPC(self):
+        for entry in self.listBox.curselection():
+            name = self.listBox.get(entry)
+            self.children[name] = EditWindow(npcs[name])
+            self.children[name].run()
+    def useNPC(self):
+        for entry in self.listBox.curselection():
+            name = self.listBox.get(entry)
+            self.children[name] = DisplayWindow(npcs[name])
+            self.children[name].run()
+
+    def makeName(self):
+        gender = self.genderBox.current()
+        equation = formatTable[self.speciesBox.current()][d20()]
+        final = ""
+        first = 0
+        for symbol in equation:
+            if("A" <= symbol < "P" or symbol == "X"):
+                sym = ord(symbol) - 65
+                part = ''
+                if(sym < 10):
+                    part = nameTable[ord(symbol) - 65][d20()]
+                elif(sym == 11 or sym == 12):
+                    if(gender == 2):
+                        gender = randint(0,1)
+                    part = nameTable[sym][d20()].split("/")[gender];
+                elif(symbol == "X"):
+                    part = str(randint(0,10000))
+                else:
+                    part = nameTable[sym][d20()].split("/")[first]
+                
+                if(first != 0):
+                    final += part.lower()
+                else :
+                    final += part
+            else:
+                final += symbol
+
+            if(symbol == " "):
+                first = 0
+            else:
+                first = 1
+        self.nameBox.delete(0,'end')
+        self.nameBox.insert(0,final)
+    def save(self):
+        with open("savedNPCs.json", "w") as saveFile:
+            pprint(npcs)
+            saveFile.write(json.dumps(npcs))
+
+    def onClose(self):
+        messagebox = MessageWindow("Do you wish to exit?", self.close, None, "Quit?", "Yes", "No")
+
+    def close(self):
+        for child,window in self.children.items():
+            window.quit()
+        self.quit()
+
+
+class NPCWindow(Application):
+    def __init__(self, npc, window, callbacks):
+        super().__init__(window, callbacks, npc['name'])
+        self.npc = copy.deepcopy(npc)
+        self.builder.get_object('nameBox').insert("end",self.npc['name'])
+        self.builder.get_object('textBox').insert("end", self.npc['text'])
+        self.builder.get_object('genderBox').set(self.npc['gender'])
+        self.builder.get_object('speciesBox').set(self.npc['species'])
+        for trait,value in self.npc['traits'].items():
+            self.builder.get_object(trait).set(value)
+
+
+class EditWindow(NPCWindow):
+    def __init__(self, npc):
+        super().__init__(npc, 'editWindow', {
+                'okayClicked': self.okay,
+                'lockClicked': self.toggleLock,
+                'cancelClicked': self.quit,
+                'nameChanged': self.rename,
+                'attributeChanged': self.updateAttributes
+            })
+        self.nameBox = self.builder.get_object('nameBox')
+        self.textBox = self.builder.get_object('textBox')
+        self.genderBox = self.builder.get_object('genderBox')
+        self.speciesBox = self.builder.get_object('speciesBox')
+        self.toggleLock()
+
+    def okay(self, event = None):
+        npcs[self.npc['name']]['gender'] = self.npc['gender']
+        npcs[self.npc['name']]['species'] = self.npc['species']
+        npcs[self.npc['name']]['text'] = self.npc['text']
+        npcs[self.npc['name']]['traits'] = self.npc['traits']
+        self.quit()
+
+    def toggleLock(self, event = None):
+        if self.speciesBox.state() == ("disabled",):
+            self.speciesBox['state'] = 'readonly'
+            self.genderBox['state'] ='readonly'
+            self.nameBox['state'] = 'normal'
+            self.builder.get_object('lockButton')['text'] = "Lock"
+        else: 
+            self.speciesBox['state'] = 'disabled'
+            self.genderBox['state'] ='disabled'
+            self.nameBox['state'] = 'disabled'
+            self.builder.get_object('lockButton')['text'] = "Unlock"
+
+
+    def rename(self, event = None):
+        oldName = self.npc['name']
+        newName = self.nameBox.get()
+        if newName not in npcs:
+            self.npc['name'] = newName
+            npcs[newName] = self.npc
+            del npcs[oldName]
+        else:
+            self.nameBox.delete(0,"end")
+            self.nameBox.insert("end", oldName)
+
+    def updateAttributes(self, event = None):
+        self.npc['gender'] = self.genderBox.get()
+        self.npc['species'] = self.speciesBox.get()
+        for trait in self.npc['traits']:
+            self.npc['traits'][trait] = self.builder.get_object(trait).get()
+        self.npc['text'] = self.textBox.get("1.0","end").strip()
+
+class DisplayWindow(NPCWindow):
+    def __init__(self, npc):
+        super().__init__(npc, 'displayWindow', {
+                'threatUpdated' : updateDisplay
+            })
+        self.threatBox = self.builder.get_object('threatBox')
+        threatBox.current(0)
+        self.updateDisplay()
+    def updateDisplay(self, event = None):
+        pass
+
+class MessageWindow(Application):
+    def __init__(self, question, callbackYes, callbackNo, title = "Message Box", labelYes = 'Yes', labelNo = 'No'):
+        super().__init__('messageWindow', {
+                'yesClick' : self.yes,
+                'noClick' : self.no,
+            },title)
+        self.callbackYes = callbackYes
+        self.callbackNo = callbackNo
+        self.builder.get_object('yesButton')['text']  = labelYes
+        self.builder.get_object('noButton')['text'] = labelNo
+        self.builder.get_object('message')['text'] = question
+    def yes(self, event = None):
+        self.quit()
+        if self.callbackYes != None:
+            self.callbackYes()
+    def no(self, event = None):
+        self.quit()
+        if self.callbackNo != None:
+            self.callbackNo()
+
+
+
+
+if getattr(sys, 'frozen', False):
+    formatFile = sys._MEIPASS + "/format.csv"
+    nameFile = sys._MEIPASS + "/sorted.csv"
+else:
+    formatFile = "format.csv"
+    nameFile = "sorted.csv"
+with open(nameFile, "r") as table:
+    reader = csv.reader(table)
+    nameTable = [[c for c in r] for r in reader]
+with open(formatFile, "r") as table:
+    reader = csv.reader(table)
+    formatTable =[[c for c in r] for r in reader]
+npcs = {}
+with open("savedNPCs.json") as saveFile:
+    try:
+        npcs = json.loads(saveFile.read())
+    except:
+        pass
+def d20():
+    return randint(0,19)
 
 if __name__ == '__main__':
-    app = Application()
+    app = RootWindow()
     app.run()
-names = list(npcs)
-
-#
-#  def d20():
-#      return randint(0,19)
-#
-#  def onClose():
-#      if messagebox.askokcancel("Quit", "Do you wish to exit?"):
-#          root.destroy()
-#
-#  def addNPC():
-#      name = nameBox.get()
-#      names.append(name)
-#      npcs[name] = NPC(name, {"gender" : genderBox.get(), "species" : speciesBox.get()})
-#      listBox.insert(END, name)
-#
-#  def deleteNPC():
-#      for name in listBox.curselection():
-#          if messagebox.askokcancel("Confirm Delete", "Delete " + listBox.get(name) + "?"):
-#              npcs[listBox.get(name)].delete()
-#
-#      listBox.delete(0, END)
-#      for npc in npcs:
-#          listBox.insert(END, npc)
-#
-#  def editNPC():
-#      for entry in listBox.curselection():
-#          name = listBox.get(entry)
-#          npcs[name].edit()
-#
-#  def makeName():
-#      gender = genderBox.current()
-#      equation = formatTable[speciesBox.current()][d20()]
-#      final = ""
-#      first = 0
-#      for symbol in equation:
-#          if("A" <= symbol < "P" or symbol == "X"):
-#              sym = ord(symbol) - 65
-#              part = ''
-#              if(sym < 10):
-#                  part = nameTable[ord(symbol) - 65][d20()]
-#              elif(sym == 11 or sym == 12):
-#                  if(gender == 2):
-#                      gender = randint(0,1)
-#                  part = nameTable[sym][d20()].split("/")[gender];
-#              elif(symbol == "X"):
-#                  part = str(randint(0,10000))
-#              else:
-#                  part = nameTable[sym][d20()].split("/")[first]
-#              
-#              if(first != 0):
-#                  final += part.lower()
-#              else :
-#                  final += part
-#          else:
-#              final += symbol
-#
-#          if(symbol == " "):
-#              first = 0
-#          else:
-#              first = 1
-#      nameBox.delete(0,'end')
-#      nameBox.insert(0,final)
-#
-#  if getattr(sys, 'frozen', False):
-#      formatFile = sys._MEIPASS + "/format.csv"
-#      nameFile = sys._MEIPASS + "/sorted.csv"
-#  else:
-#      formatFile = "format.csv"
-#      nameFile = "sorted.csv"
-#  with open(nameFile, "r") as table:
-#      reader = csv.reader(table)
-#      nameTable = [[c for c in r] for r in reader]
-#  with open(formatFile, "r") as table:
-#      reader = csv.reader(table)
-#      formatTable =[[c for c in r] for r in reader]
-#
-#  generatedName = ''
-#  speciesBox.current(0)
-#  speciesBox.bind("<<ComboboxSelected>>", setGenderBox)
-#  genderBox.current(0)
-#
-#  for name in names:
-#      listBox.insert(END, name)
-#      npcs[name] = NPC(name, npcs[name])
-#
--- nameGen/npc.py
+++ nameGen/npc.py
@@ -1,82 +1,15 @@
-from tkinter import *
-import json
-
-npcs = {}
-with open("savedNPCs.json") as saveFile:
-    npcs = json.loads(saveFile.read())
-
-def save():
-    with open("savedNPCs.json", "w") as saveFile:
-        saveFile.write(json.dumps(npcs))
-
 class NPC(dict):
     def __init__(self, name, data):
         dict.__init__(self)
-        self.name = name
-        self.gender = data['gender'] if 'gender' in data else "N"
-        self.species = data['species'] if 'species' in data else "Undefined"
-        self.text = data['text'] if 'text' in data else ""
-        self.window = None
-        self.frame = None
-        self.entry = None
-        self.genderBox = None
-        self.speciesBox = None
-        self.traits = {
+        self['name'] = name
+        self['gender'] = data['gender'] if 'gender' in data else "N"
+        self['species'] = data['species'] if 'species' in data else "Undefined"
+        self['text'] = data['text'] if 'text' in data else ""
+        self['traits'] = data['traits'] if 'traits' in data else {
             'initiative' : int(data['initiative']) if 'initiative' in data else 1,
             'defense' : int(data['defense']) if 'defense' in data else 1,
             'attack' : int(data['attack']) if 'attack' in data else 1,
             'resilience' : int(data['resilience']) if 'resilience' in data else 1,
             'competence' : int(data['competence']) if 'competence' in data else 1,
             'health' : int(data['health']) if 'health' in data else 1,
-            'signatureSkills' : int(data['defense']) if 'defense' in data else None
         }
-
-    def closeWindow(self):
-        self.window.destroy()
-        self.window = None
-        self.frame = None
-        self.entry = None
-
-    def saveChanges(self):
-        npcs[self.name]['text'] = self.entry.get(1.0, END)
-        npcs[self.name]['gender'] = self.genderBox.get()
-        npcs[self.name]['species'] = self.speciesBox.get()
-
-    def saveAndClose(self):
-        self.saveChanges()
-        self.closeWindow()
-
-    def edit(self):
-        self.window = Toplevel()
-        self.window.title(self.name)
-        self.frame = ttk.Frame(self.window, padding="3 3 12 12")
-        self.frame.grid(column=0, row=0)
-        self.entry = Text(self.window, width=30, height = 6)
-        self.entry.insert(END, self.text)
-        self.genderBox = ttk.Combobox(self.frame, values = ['Male', 'Female', 'Either/Both/None'], state = ['readonly'])
-        self.speciesBox = ttk.Combobox(self.frame, values = ['Drake', 'Dwarf', 'Elf', 'Giant', 'Goblin', 'Human', 'Ogre', 'Orc', 'Pech', 'Rootwalker', 'Saurian', 'Unborn'], state=['readonly'])
-
-        self.speciesBox.grid(column = 0, row = 0)
-        self.genderBox.grid(column = 0, row = 1)
-        self.genderBox.set(self.gender)
-        self.speciesBox.set(self.species)
-
-        ttk.Button(self.frame, text="Save and Close", command=self.saveAndClose).grid(column = 0, row = 2)
-        ttk.Button(self.frame, text="Cancel Edit", command=self.closeWindow).grid(column = 0, row = 3)
-        self.entry.grid(column =2, row = 0)
-    
-    def delete(self):
-        del npcs[self.name]
-        self.name = None
-        self.gender = None
-        self.species = None
-        self.text = None
-        if self.window:
-            self.window.destroy()
-        self.window = None
-        self.frame = None
-        self.entry = None
-        self.genderBox = None
-        self.speciesBox = None
-        for trait in self.traits:
-            self.traits[trait] = None
